d227598f98f58ef27098e7010dba4c6dfa3a4ce0
Sam Grigg <griggsn@ldschurch.org>
2010-09-30 23:08:38 -0600

    Initial app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '83e91af5105ba0c8b4fe3864145552961efd9a0e67cf4f7d0067b26f4abed5115359c7747a9cb81aab9d7832159ad39f014ed1de54fd7a1ed996abfb2e3d40dc'
